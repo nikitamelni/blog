4842a8c755a1d48da062f4595703ddbea9189d62
Nikita Melnichenko <17317307+nikitamelni@users.noreply.github.com>
2021-08-11 11:10:21 -0400

    working navigation, but only in the header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -6,28 +6,16 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import Home from './Components/Home'
-import Contacts from './Components/Contacts'
-import Posts from './Components/Posts'
 import Header from './Components/Header/Header'
 
 function App() {
-  return (
-    <Router>
-      <Header/>
-      <Switch>
-        <Route path="/contacts">
-          <h1>Contacts</h1>
-        </Route>
-        <Route path="/posts">
-          <Posts />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+  const [count, setCount] = useState(0);
 
+  return (
+    <>
+      <Header/>
+
+    </>
   );
 }
 
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,8 +4,11 @@ import {
     Switch,
     Route,
     Link
-  } from "react-router-dom";
+} from "react-router-dom";
 import './Header.css';
+import Home from '../../Components/Home'
+import Contacts from '../../Components/Contacts'
+import Posts from '../../Components/Posts'
 
 function Header() {
     return (
@@ -14,24 +17,36 @@ function Header() {
                 <nav class="navigationMenu">
                     <ul>
                         <li className="navigationButton">
-                          <Link to="/">Home</Link>
-                          <svg viewBox="0 0 500 150" preserveAspectRatio="none">
-                          <path fill="none" d="M325,18C228.7-8.3,118.5,8.3,78,21C22.4,38.4,4.6,54.6,5.6,77.6c1.4,32.4,52.2,54,142.6,63.7 c66.2,7.1,212.2,7.5,273.5-8.3c64.4-16.6,104.3-57.6,33.8-98.2C386.7-4.9,179.4-1.4,126.3,20.7" />
-                        </svg></li>
+                            <Link to="/">Home</Link>
+                            <svg viewBox="0 0 500 150" preserveAspectRatio="none">
+                                <path fill="none" d="M325,18C228.7-8.3,118.5,8.3,78,21C22.4,38.4,4.6,54.6,5.6,77.6c1.4,32.4,52.2,54,142.6,63.7 c66.2,7.1,212.2,7.5,273.5-8.3c64.4-16.6,104.3-57.6,33.8-98.2C386.7-4.9,179.4-1.4,126.3,20.7" />
+                            </svg></li>
                         <li className="navigationButton">
                             <Link to="/posts">Posts</Link>
                             <svg viewBox="0 0 500 150" preserveAspectRatio="none">
-                            <path fill="none" d="M325,18C228.7-8.3,118.5,8.3,78,21C22.4,38.4,4.6,54.6,5.6,77.6c1.4,32.4,52.2,54,142.6,63.7 c66.2,7.1,212.2,7.5,273.5-8.3c64.4-16.6,104.3-57.6,33.8-98.2C386.7-4.9,179.4-1.4,126.3,20.7" />
-                        </svg></li>
+                                <path fill="none" d="M325,18C228.7-8.3,118.5,8.3,78,21C22.4,38.4,4.6,54.6,5.6,77.6c1.4,32.4,52.2,54,142.6,63.7 c66.2,7.1,212.2,7.5,273.5-8.3c64.4-16.6,104.3-57.6,33.8-98.2C386.7-4.9,179.4-1.4,126.3,20.7" />
+                            </svg></li>
                         <li className="navigationButton">
                             <Link to="/contacts">Contacts</Link>
                             <svg viewBox="0 0 500 150" preserveAspectRatio="none">
-                            <path fill="none" d="M325,18C228.7-8.3,118.5,8.3,78,21C22.4,38.4,4.6,54.6,5.6,77.6c1.4,32.4,52.2,54,142.6,63.7 c66.2,7.1,212.2,7.5,273.5-8.3c64.4-16.6,104.3-57.6,33.8-98.2C386.7-4.9,179.4-1.4,126.3,20.7" />
-                        </svg></li>
+                                <path fill="none" d="M325,18C228.7-8.3,118.5,8.3,78,21C22.4,38.4,4.6,54.6,5.6,77.6c1.4,32.4,52.2,54,142.6,63.7 c66.2,7.1,212.2,7.5,273.5-8.3c64.4-16.6,104.3-57.6,33.8-98.2C386.7-4.9,179.4-1.4,126.3,20.7" />
+                            </svg></li>
                     </ul>
                 </nav>
+                <hr />
+                <Switch>
+                    <Route exact path="/contacts">
+                        <Contacts />
+                    </Route>
+                    <Route exact path="/posts">
+                        <Posts />
+                    </Route>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                </Switch>
             </Router>
-            <hr />
+
         </>
     );
 }
